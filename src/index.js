173a56f8b66c30aac99e6ad68b963dd611808877
Kaisaan <kaisaansulemana@gmail.com>
2025-03-12 13:10:31 +0000

    Added userSchema and connected to db on startup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
+
+async function connectToDB(){
+    try {
+        await mongoose.connect(process.env["SWAPSHELFDB"], {});
+        console.log("Connection to DB successfull");
+    } catch (error) {
+        console.log("Connection to DB error: ", error);
+        process.exit(1)
+    }
+}
 
 app.get('/',(req, res) => {
     res.status(200).json({
@@ -11,5 +22,6 @@ app.get('/',(req, res) => {
 })
 
 app.listen(PORT, () => {
+    connectToDB()
     console.log("Server listening on port " + PORT);
 })
